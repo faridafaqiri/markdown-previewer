5a0653c93856fabf0b49027ea10c1cdb94ee71c8
far_faqiri <farida.faqiri2@gmail.com>
2024-08-22 17:50:40 +0430

    feat: Initialize React Markdown Previewer project with basic setup

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './App.css';
+
+function App() {
+  const [markdown, setMarkdown] = useState(`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine === \\\`\\\`\\\` && lastLine === \\\`\\\`\\\`) {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+`);
+
+  return (
+    <div className="app">
+      <nav className="navbar">
+        <h2>Editor</h2>
+      </nav>
+      <textarea
+        id="editor"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+
+      <nav className="navbar">
+        <h2>Reviewer</h2>
+      </nav>
+
+      <div id="preview">
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+    </div>
+  );
+}
+
+export default App;
